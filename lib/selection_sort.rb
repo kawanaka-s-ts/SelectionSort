6c7e296960a3678dbaf78668ee6677350ef4070d
kawanaka <kawanaka_s@tenso.com>
2019-03-04 15:36:38 +0900

    作成し直し
    - Array#indexを使用しない
    - Array#minを使用しない

--- lib/selection_sort.rb
+++ lib/selection_sort.rb
@@ -1,46 +1,36 @@
 class SelectionSort
   def asc_order(numbers)
+    # indexに0を代入する
     index = 0
-    # indexの値が要素数-1より小さい間繰り返す
-    while index < numbers.size - 1
-      # rangeは現在のindexより上のindexすべて
-      range = (index + 1)..numbers.size
-      # 最小値のindexを取得
-      min_index = numbers.index(min_search(numbers[range]))
-      # numbers[index]よりnumbers[min_index]小さい時
-      if numbers[index] > numbers[min_index]
-        # 要素を入れ替える
-        # hold_valueにnumbers[index]の値を保存
-        hold_value = numbers[index]
-        # numbers[index]にnumbers[min_index]の値を代入
-        numbers[index] = numbers[min_index]
-        # numbers[min_index]にhold_valueに保持していた値を代入
-        numbers[min_index] = hold_value
+    # indexの値が要素数より小さい間繰り返す
+    while index < numbers.size
+      # jはindexに1を加えたもの
+      j = index + 1
+      # jが要素数より小さい間繰り返す
+      while j < numbers.size
+        # min_indexは最小値のindex
+        # min_indexにindexを代入
+        min_index = index
+        # numbers[index]を仮の最小値とする
+        # 仮の最小値よりもnumbers[j]の方が小さければ
+        if numbers[index] > numbers[j]
+          # 最小値のindexにjの値を代入
+          min_index = j
+        # ifここまで
+        end
+        # jを1増やす
+        j += 1
+        #  もし仮の最小値よりもnumbers[min_index]の方が小さければ
+        if numbers[index] > numbers[min_index]
+          # 要素を入れ替える
+          numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
+        # ifここまで
+        end
       end
       # indexを1増やす
       index += 1
     end
     # 並べ替えた配列を返す
     numbers
-  end
-
-  def min_search(numbers)
-    # iはindex
-    i = 1
-    # minは最小値
-    # 配列の最初の数値を仮の最小値としてminに代入
-    min = numbers[0]
-    # iが要素数を超えるまで繰り返す
-    while i < numbers.size
-      # もし仮の最小値よりnumbers[i]の方が小さい時
-      if min > numbers[i]
-        # numbers[i]をminに代入
-        min = numbers[i]
-      end
-      # iを1増やす
-      i += 1
-    end
-    # 最小値を返す
-    min
   end
 end
--- spec/selection_sort_spec.rb
+++ spec/selection_sort_spec.rb
@@ -8,21 +8,15 @@ describe SelectionSort do
       numbers = [*1..10].shuffle
       expect(selection_sort.asc_order(numbers)).to eq [*1..10]
     end
-  end
 
-  describe "#min_search" do
-    context "複数の要素を持った配列が渡された時" do
-      it "配列内の最小値を返す" do
-        numbers = [5, 4, 1, 2, 3]
-        expect(selection_sort.min_search(numbers)).to eq 1
-      end
+    it "要素を昇順に並び替えた配列を返す" do
+      numbers = [0, 1]
+      expect(selection_sort.asc_order(numbers)).to eq [0, 1]
     end
 
-    context "配列内に同じ値が複数ある場合" do
-      it "配列内の最小値を返す" do
-        numbers = [5, 5, 4, 4, 1, 1, 2, 2, 3, 3]
-        expect(selection_sort.min_search(numbers)).to eq 1
-      end
+    it "要素を昇順に並び替えた配列を返す" do
+      numbers = [1, 0]
+      expect(selection_sort.asc_order(numbers)).to eq [0, 1]
     end
   end
 end
