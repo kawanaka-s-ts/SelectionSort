55aba7a0c592605b86888e5e7d30db92afe06fa8
kawanaka <kawanaka_s@tenso.com>
2019-03-05 09:48:23 +0900

    fixup! 作成し直し
    - 変数jの説明変更
    - 条件式の記述変更
    - 必要のない条件式削除

--- lib/selection_sort.rb
+++ lib/selection_sort.rb
@@ -4,7 +4,7 @@ class SelectionSort
     index = 0
     # indexの値が要素数より小さい間繰り返す
     while index < numbers.size
-      # jはindexに1を加えたもの
+      # jは比較対象の数値のindex
       j = index + 1
       # jが要素数より小さい間繰り返す
       while j < numbers.size
@@ -13,19 +13,15 @@ class SelectionSort
         min_index = index
         # numbers[index]を仮の最小値とする
         # 仮の最小値よりもnumbers[j]の方が小さければ
-        if numbers[index] > numbers[j]
+        if numbers[j] < numbers[index]
           # 最小値のindexにjの値を代入
           min_index = j
         # ifここまで
         end
         # jを1増やす
         j += 1
-        #  もし仮の最小値よりもnumbers[min_index]の方が小さければ
-        if numbers[index] > numbers[min_index]
-          # 要素を入れ替える
-          numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
-        # ifここまで
-        end
+        # 要素を入れ替える
+        numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
       end
       # indexを1増やす
       index += 1
