85bf32f00825b72b6caca362e3b4029f36e26e1d
kawanaka <kawanaka_s@tenso.com>
2019-03-06 14:50:14 +0900

    アルゴリズムを修正

--- lib/selection_sort.rb
+++ lib/selection_sort.rb
@@ -4,25 +4,25 @@ class SelectionSort
     index = 0
     # indexの値が要素数より小さい間繰り返す
     while index < numbers.size
+      # min_indexは最小値のindex
+      # numbers[index]を仮の最小値とする
+      # indexを代入
+      min_index = index
       # jは比較対象の数値のindex
       j = index + 1
       # jが要素数より小さい間繰り返す
       while j < numbers.size
-        # min_indexは最小値のindex
-        # min_indexにindexを代入
-        min_index = index
-        # numbers[index]を仮の最小値とする
         # 仮の最小値よりもnumbers[j]の方が小さければ
-        if numbers[j] < numbers[index]
+        if numbers[j] < numbers[min_index]
           # 最小値のindexにjの値を代入
           min_index = j
         # ifここまで
         end
         # jを1増やす
         j += 1
-        # 要素を入れ替える
-        numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
       end
+      # 要素を入れ替える
+      numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
       # indexを1増やす
       index += 1
     end
